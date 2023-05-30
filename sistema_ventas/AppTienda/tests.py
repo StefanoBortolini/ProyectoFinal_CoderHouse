68055d2763c8c877e2aab16332ba3de73a0ac384
Stefano Bortolini <stefanobortolini@hotmail.com>
2023-05-29 23:53:46 -0300

    Creado y testeado el archivo test.py en AppTienda

--- sistema_ventas/AppTienda/tests.py
+++ sistema_ventas/AppTienda/tests.py
@@ -1,3 +1,18 @@
+# Importamos la clase TestCase, el modelo Posteo y el cliente de prueba
 from django.test import TestCase
+from AppTienda.models import Posteo
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class PosteoModelTest(TestCase):
+
+    def setUp(self):
+        user = User.objects.create_user(username="usuario1", email="usuario1@gmail.com", password="123456")
+        Posteo.objects.create(user=user, marca="Ford", modelo="Fiesta", year=2010, kilometros=50000, descripcion="Buen estado", email="usuario1@gmail.com", telefono=123456789, precio=100000)
+
+    def test_posteo_year(self):
+        posteo = Posteo.objects.get(id=1)
+        self.assertEqual(posteo.year, 2010)
+
+    def test_posteo_precio(self):
+        posteo = Posteo.objects.get(id=1)
+        self.assertEqual(posteo.precio, 100000)
